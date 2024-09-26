6655d13828e6249d0cd44091d333d063baa0a6b5
Yud <yudhafebrirastuama@gmail.com>
2024-09-26 21:14:18 +0700

    Command in push.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,5 +1,11 @@
-docker build -t item-app:v1
+docker build -t item-app:v1 .
+
+# Menampilkan semua image
 docker image ls -a
+
+# Merubah nama
 docker tag my-image:v1 yufebra/item-app:v1
+
+# Melakukan login ke docker dengan command
 docker login
 docker push yufebra/item-app:v1
